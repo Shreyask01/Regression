76338da62c35320a2634fd6ee133a5fc90147aaf
shreyas <shreyaskolambakar@gmail.com>
2019-07-23 19:19:05 +0530

    regression test suite

--- src/TestCase/AVS.java
+++ src/TestCase/AVS.java
@@ -34,7 +34,10 @@ public class AVS {
 		// Create Estimate AVS
 		Thread.sleep(5000);
 		Estimatedetails e = new Estimatedetails(driver);
-		e.estimatename().sendKeys("avs1302");
+		
+	
+		
+		e.estimatename().sendKeys("demoest1");
 		e.client().sendKeys("avs11s21");
 		e.opportunityID().sendKeys("121");
 		e.solution().sendKeys("2111");
@@ -45,13 +48,28 @@ public class AVS {
 		Thread.sleep(5000);
 		driver.findElement(By.xpath("//button[@type='submit']")).click();
 		
+		//enter client details pop-up
 		Thread.sleep(20000);
 		driver.findElement(By.xpath("//md-select[@name='industry']")).click();
 		//driver.findElement(By.xpath("//md-select-value[@id='select_value_label_106']")).click();
 		driver.findElement(By.xpath("//*[text()='PD Auto']")).click();
+		Thread.sleep(2000);
 		
 		driver.findElement(By.xpath("//md-select[@name='stage']")).click();
+		driver.findElement(By.xpath("//*[text()='2B']")).click();
+		Thread.sleep(2000);
 		
+		driver.findElement(By.xpath("//md-select[@name='deal']")).click();
+		driver.findElement(By.xpath("//*[text()='Contract Renewal']")).click();
+		Thread.sleep(2000);
 		
+		driver.findElement(By.xpath("//button[@type='submit']")).click();
+		
+		Thread.sleep(3000);
+		
+		//select modules from modules tab
+		driver.findElement(By.xpath("//h5[text()='M3 Standard']")).click();
+		driver.findElement(By.xpath("//h5[text()='Module 3']")).click();
+		driver.findElement(By.xpath("//h5[text()='Module 7']")).click();
 	}
 }
